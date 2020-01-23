c15096256036b1d41b46a9dccb2108ef7be3f430
Farhana Yasmin <farhana.yasmin@mobile-di.com>
2020-01-23 13:41:02 +0530

    initial commit

---
+++ app/routes/router.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+var fs = require('fs');
+const https = require('https');
+const axios = require('axios');
+var CircularJSON = require('circular-json')
+const JSPON = require('jspon');
+var mongoose = require('mongoose');
+
+var db = mongoose.connection
+db.createCollection('gitData');
+	var dataSaved = db.collection('gitDaata');
+
+router.post('/postData',function(request,response){
+    axios.get('https://api.github.com/users/mralexgray/repos')
+  .then(URLdata => {
+     const str = CircularJSON.stringify(URLdata);
+     
+    var data=JSON.parse(str)
+    
+   response.send({
+       "data":data
+    }) 
+    dataSaved.bulkWrite(data, function(err) {
+        console.log("data saved in DB")
+        if (err) throw err;
+    })
+
+  })
+  .catch(error => {
+    console.log(error);
+  });
+    
+})
+router.get('/getData',function(request,response){
+    var id=request.query.id
+   var dataById= dataSaved.find({"id":id})
+   response.send({
+       "data":dataById
+   })
+
+ 
+})
+
+module.exports = router;
